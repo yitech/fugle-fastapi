c5f0c45c4bc3081cbc79aaf956ef2d5796aa9e93
yite <yite@icloud.com>
2024-07-28 08:48:22 +0800

    ignore idea

--- example.py
+++ example.py
@@ -2,6 +2,8 @@ from configparser import ConfigParser
 from fugle_trade.sdk import SDK
 from fugle_trade.order import OrderObject
 from fugle_trade.constant import (APCode, Trade, PriceFlag, BSFlag, Action)
+from dotenv import load_dotenv
+import os
 # 讀取設定檔
 config = ConfigParser()
 config.read('./config.ini')
@@ -10,13 +12,15 @@ sdk = SDK(config)
 sdk.login()
 # 建立委託物件
 order = OrderObject(
-    buy_sell = Action.Buy,
-    price_flag = PriceFlag.Limit,
-    bs_flag = BSFlag.ROD,
-    price = 1200,
-    stock_no = "2454",
-    quantity = 40,
-    ap_code = APCode.IntradayOdd
+    buy_sell=Action.Buy,
+    price_flag=PriceFlag.Limit,
+    bs_flag=BSFlag.ROD,
+    price=1200,
+    stock_no="2454",
+    quantity=40,
+    ap_code=APCode.IntradayOdd
 )
-res = sdk.place_order(order)
+
+res = sdk.get_balance()
+# res = sdk.place_order(order)
 print(res)
