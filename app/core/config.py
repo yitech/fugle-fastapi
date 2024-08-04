ec6eb79faac1274c154a3209944849595dd62f8a
yite <yite@icloud.com>
2024-08-04 20:35:54 +0800

    load balancer

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     fugle_market_api_key: str
     fugle_trade_config: str
     api_secret: Optional[str] = None
+    domain: Optional[str] = None
+    email: Optional[str] = None
 
     class Config:
         env_file = ".env"
