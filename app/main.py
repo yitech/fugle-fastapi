cf1dce99d309677f8013730a364952ef70d42c39
yite <yite@icloud.com>
2024-08-11 16:12:31 +0800

    use log format

--- app/main.py
+++ app/main.py
@@ -5,66 +5,17 @@ from app.api.v1 import order
 from app.dependencies.fugle import TraderSingleton
 from app.middleware.auth import middleware
 import logging
-import sys
+from pathlib import Path
+import json
 
-# Define your custom logging configuration
-# Define the logging configuration
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "format": "[%(levelname)s] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s"
-        },
-        "json": {
-            "format": "{\"level\": \"%(levelname)s\", \"time\": \"%(asctime)s\", \"filename\": \"%(filename)s\", \"lineno\": \"%(lineno)d\", \"message\": \"%(message)s\"}"
-        }
-    },
-    "handlers": {
-        "default": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "default",
-            "stream": "ext://sys.stdout",
-            "formatter": "default"
-        },
-        "file": {
-            "level": "INFO",
-            "class": "logging.handlers.TimedRotatingFileHandler",
-            "formatter": "json",
-            "filename": "log/fugle-fastapi.log",  # Base file name
-            "when": "midnight",  # Rotate at midnight
-            "backupCount": 10,  # Keep 10 days of logs
-            "encoding": "utf-8",
-            "delay": False,
-            "utc": False,
-        },
-    },
-    "loggers": {
-        "fugle": {
-            "handlers": ["default", "file"],
-            "level": "INFO",
-            "propagate": False
-        },
-        "uvicorn": {
-            "handlers": ["default", "file"],
-            "level": "INFO",
-            "propagate": False
-        },
-        "uvicorn.error": {
-            "handlers": ["default", "file"],
-            "level": "INFO",
-            "propagate": False
-        },
-        "uvicorn.access": {
-            "handlers": ["default", "file"],
-            "level": "INFO",
-            "propagate": False
-        }
-    }
-}
-
-logging.config.dictConfig(logging_config)
+# Load logging configuration from JSON file
+log_config_path = Path("logging_config.json")
+if log_config_path.is_file():
+    with open(log_config_path, "r") as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+else:
+    raise FileNotFoundError(f"Logging configuration file not found: {log_config_path}")
 
 # Create a logger for your application
 logger = logging.getLogger("fugle")
