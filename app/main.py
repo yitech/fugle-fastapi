fcbae85defa952ccfd8a8cea7a64a98574092f28
yite <yite@icloud.com>
2024-08-11 16:09:14 +0800

    add json format logger

--- app/main.py
+++ app/main.py
@@ -8,50 +8,66 @@ import logging
 import sys
 
 # Define your custom logging configuration
+# Define the logging configuration
 logging_config = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
         "default": {
-            "format": "[%(levelname)s] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s",
+            "format": "[%(levelname)s] [%(asctime)s] [%(filename)s:%(lineno)d] %(message)s"
         },
+        "json": {
+            "format": "{\"level\": \"%(levelname)s\", \"time\": \"%(asctime)s\", \"filename\": \"%(filename)s\", \"lineno\": \"%(lineno)d\", \"message\": \"%(message)s\"}"
+        }
     },
     "handlers": {
         "default": {
             "level": "INFO",
             "class": "logging.StreamHandler",
-            "stream": sys.stdout,
             "formatter": "default",
+            "stream": "ext://sys.stdout",
+            "formatter": "default"
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "formatter": "json",
+            "filename": "log/fugle-fastapi.log",  # Base file name
+            "when": "midnight",  # Rotate at midnight
+            "backupCount": 10,  # Keep 10 days of logs
+            "encoding": "utf-8",
+            "delay": False,
+            "utc": False,
         },
     },
     "loggers": {
-        "": {  # Root logger
-            "handlers": ["default"],
+        "fugle": {
+            "handlers": ["default", "file"],
             "level": "INFO",
-            "propagate": False,  # Prevent log message propagation to other loggers
+            "propagate": False
         },
         "uvicorn": {
-            "handlers": ["default"],
+            "handlers": ["default", "file"],
             "level": "INFO",
-            "propagate": False,  # Prevent duplicate logs from uvicorn
+            "propagate": False
         },
         "uvicorn.error": {
+            "handlers": ["default", "file"],
             "level": "INFO",
-            "handlers": ["default"],
-            "propagate": False,  # Prevent duplicate logs from uvicorn.error
+            "propagate": False
         },
         "uvicorn.access": {
-            "handlers": ["default"],
+            "handlers": ["default", "file"],
             "level": "INFO",
-            "propagate": False,  # Prevent duplicate logs from uvicorn.access
-        },
-    },
+            "propagate": False
+        }
+    }
 }
 
 logging.config.dictConfig(logging_config)
 
 # Create a logger for your application
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("fugle")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
