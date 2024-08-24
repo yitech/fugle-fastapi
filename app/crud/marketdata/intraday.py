0baecc70c1db95d65b656e2f8767f5753a6b52d6
yite <yite@icloud.com>
2024-08-24 14:40:53 +0800

    remove logger

--- app/crud/marketdata/intraday.py
+++ app/crud/marketdata/intraday.py
@@ -1,12 +1,8 @@
 from typing import Literal
 from app.dependencies.fuglemarket import MarketSingleton
 from app.schema.intraday import Quote
-import logging
-
-logger = logging.getLogger("fugle")
 
 def get_stock_quote(market: MarketSingleton, symbol: str, kind: Literal["oddlot", "EQUITY"]="EQUITY") -> Quote:
     res = market.get_stock_quote(symbol, kind)
-    logger.info(f"Quote for {symbol}: {res}")
     return res
 
