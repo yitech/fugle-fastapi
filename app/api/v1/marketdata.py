b7a0a418442f3328bc3e3b39a9cc48153c1095f2
yite <yite@icloud.com>
2024-08-24 15:22:24 +0800

    update historical data

--- app/api/v1/marketdata.py
+++ app/api/v1/marketdata.py
@@ -1,8 +1,14 @@
 from typing import Literal, Optional
 from fastapi import APIRouter, Depends, HTTPException
-from app.schema.fuglemarket import QuoteResponse
+from app.schema.fuglemarket import (
+    QuoteResponse, 
+    KLinesResponse
+)
 from app.dependencies import get_market
-from app.crud.marketdata import get_intraday_quote
+from app.crud.marketdata import (
+    get_intraday_quote,
+    get_historical_candles
+)
 
 router = APIRouter()
 
@@ -14,3 +20,11 @@ def get_quote(symbol: str, type: Optional[Literal["oddlot", "EQUITY"]]="EQUITY",
         return HTTPException(status_code=501, detail=str(e))
     return res
 
+
+@router.get("/historical/candles", response_model=KLinesResponse)
+def get_candles(symbol: str, from_date: str, to_date: str, resolution: Optional[str]="D", market=Depends(get_market)):
+    try:
+        res = get_historical_candles(market, symbol, from_date, to_date, resolution)
+    except ValueError as e:
+        return HTTPException(status_code=501, detail=str(e))
+    return res
--- app/crud/marketdata.py
+++ app/crud/marketdata.py
@@ -1,8 +1,9 @@
 from typing import Literal
 from app.dependencies.fuglemarket import MarketSingleton
-from app.schema.fuglemarket import QuoteResponse
+from app.schema.fuglemarket import QuoteResponse, KLinesResponse
 
 def get_intraday_quote(market: MarketSingleton, symbol: str, kind: Literal["oddlot", "EQUITY"]="EQUITY") -> QuoteResponse:
     return market.get_intraday_quote(symbol, kind)
 
-
+def get_historical_candles(market: MarketSingleton, symbol: str, from_date: str, to_date: str, resolution: str="D") -> KLinesResponse:
+    return market.get_historical_candles(symbol, from_date, to_date, resolution)
