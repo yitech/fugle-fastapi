bbbf21a29262344c32c1cec720a9676edef4b0ea
yite <yite@icloud.com>
2024-09-04 22:01:02 +0800

    remove credentials

--- test_marketdata.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from unittest.mock import patch, MagicMock
-from fugle_marketdata import RestClient
-import json
-
-client = RestClient(api_key="ZjZjOTU1ZjctYjdlZC00OWUzLThiOGQtZjg1MDcwMThhYzBkIDU3NWNjOGQ1LWY3NGUtNDJmOS05MDdjLTRiOTViMjM1ZTIwZg==")
-res = client.stock.historical.candles(symbol="2330", **{"from":"2024-08-20", "to":"2024-08-24", "timeframe":"D"})
-print(json.dumps(res, indent=2))
