761512c3a2600db0fd9c31879fc3e5e9f3d8a3d8
yite <yite@icloud.com>
2024-09-19 22:46:36 +0800

    rename order to trader

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,5 +1,5 @@
 from .marketdata import get_historical_candles, get_intraday_quote
-from .order import (
+from .trader import (
     create_order, get_order_results, cancel_order, get_market_status
 )
 
