0a04c8340c1919cce5e913ac14bcddc14962279b
yite <yite@icloud.com>
2024-09-19 22:38:42 +0800

    init wallet endpoints

--- app/api/v1/wallet.py
+++ app/api/v1/wallet.py
@@ -6,5 +6,17 @@ logger = logging.getLogger("fugle")
 router = APIRouter()
 
 
+@router.get("/settlements", response_model="Settlements")
+def get_settlements():
+    return {"message": "Not implemented yet"}
 
+
+@router.get("/balance", response_model="Balance")
+def get_balance():
+    return {"message": "Not implemented yet"}
+
+
+@router.get("/inventories", response_model="Inventories")
+def get_inventories():
+    return {"message": "Not implemented yet"}
 
